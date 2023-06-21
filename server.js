8fedc463327550a12a40ffec9d5a88b4d87a1395
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-22 00:17:46 +0200

    Fetch or Get Data from Database

--- server.js
+++ server.js
@@ -5,19 +5,29 @@ const app = express();
 
 app.use(express.json());
 
-
-
-
-
 //routes
 
-app.post('/product', async (req, res) => {
+app.get('/products', async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+
+
+
+
+app.post('/products', async (req, res) => {
     try {
       const product = await Product.create(req.body);
       res.status(200).json(product);
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ message: error.message });
+
+       } catch (error) {
+        console.log(error.message);
+         res.status(500).json({ message: error.message });
     }
   });
 
