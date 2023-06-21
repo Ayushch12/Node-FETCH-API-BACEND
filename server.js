06c337319c7fbb04a2c75656d2e52c133c99929b
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-21 22:03:09 +0200

    Connect to MangoosDB

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const { error } = require('console')
 const express = require ('express')
+const { default: mongoose } = require('mongoose')
+const mangoose = require('mongoose')
 const app = express()
 
 
@@ -8,7 +11,20 @@ app.get('/', (req, res)=>{
     res.send('Hello NODE API')
 })
 
+app.get('/blog', (req, res)=>{
+    res.send('Hello Blog My name is Devtamin')
+})
 
-app.listen(3000, () =>{
-    console.log(`Node Api is running on port 3000`)
+
+
+mongoose.connect
+('mongodb+srv://ayushchalise93:Ayush321XXEEt@nodeapi.fp0d1ag.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    app.listen(3000, () =>{
+        console.log(`Node Api is running on port 3000`)
+    })
+
+    console.log('connected to MongoDB')
+}).catch((error) =>{
+    console.log(error)
 })
