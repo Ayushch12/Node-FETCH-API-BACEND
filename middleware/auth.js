938613670b2aebcbc9948f1ddefec17f41d8f32d
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-22 17:06:12 +0200

    Updated the project

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { secretKey } = require('./config');
 
 
--- server.js
+++ server.js
@@ -19,6 +19,64 @@
 
 
 
+// // Importing MongoDB and other required packages:
+// const connectDB = require('./mongoDB/db');
+// const productRoutes = require('./routes/productRoutes');
+// const express = require('express');
+// const socketIO = require('socket.io');
+// const http = require('http');
+// const jwt = require('jsonwebtoken');
+// const { applyMiddleware, createStore } = require('redux');
+// const thunkMiddleware = require('redux-thunk').default;
+// const { authenticateToken } = require('./middleware/auth');
+
+// const app = express();
+// app.use(express.json());
+
+// // Create a server with http for socket.io:
+// const server = http.createServer(app);
+// const io = socketIO(server);
+
+// // Define the routes and other middleware:
+// connectDB().then(() => {
+//   server.listen(3000, () => {
+//     console.log('Node API is running on port 3000');
+//   });
+// });
+
+// // Apply Redux middleware:
+// const store = createStore(() => {}, applyMiddleware(thunkMiddleware));
+
+// // Socket.io configuration:
+// io.use((socket, next) => {
+//   if (socket.handshake.query && socket.handshake.query.token) {
+//     jwt.verify(socket.handshake.query.token, ' secretKey', (err, decoded) => {
+//       if (err) return next(new Error('Authentication error'));
+//       socket.decoded = decoded;
+//       next();
+//     });
+//   } else {
+//     next(new Error('Authentication error'));
+//   }
+// }).on('connection', (socket) => {
+//   console.log('New socket connection:', socket.id);
+//   // Handle socket events here
+// });
+
+// // Middleware to include authentication token in request headers
+// app.use((req, res, next) => {
+//   const token = req.header('Authorization');
+//   if (token) {
+//     req.headers.authorization = token;
+//   }
+//   next();
+// });
+
+// app.use('/products', authenticateToken, productRoutes);
+
+// module.exports = app;
+
+
 // Importing MongoDB and other required packages:
 const connectDB = require('./mongoDB/db');
 const productRoutes = require('./routes/productRoutes');
@@ -29,6 +87,7 @@ const jwt = require('jsonwebtoken');
 const { applyMiddleware, createStore } = require('redux');
 const thunkMiddleware = require('redux-thunk').default;
 const { authenticateToken } = require('./middleware/auth');
+const { secretKey } = require('./middleware/config');
 
 const app = express();
 app.use(express.json());
@@ -50,7 +109,7 @@ const store = createStore(() => {}, applyMiddleware(thunkMiddleware));
 // Socket.io configuration:
 io.use((socket, next) => {
   if (socket.handshake.query && socket.handshake.query.token) {
-    jwt.verify(socket.handshake.query.token, ' secretKey', (err, decoded) => {
+    jwt.verify(socket.handshake.query.token, secretKey, (err, decoded) => {
       if (err) return next(new Error('Authentication error'));
       socket.decoded = decoded;
       next();
