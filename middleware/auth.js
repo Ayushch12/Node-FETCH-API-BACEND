3f93a4e3cdb3e6b4a85d6c04cbda05ca03b98abd
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-22 11:41:39 +0200

    Working on Socket,Authentification JWT/Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,22 @@
 
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.status(401).json({ message: 'Authentication required' });
+  }
+
+  jwt.verify(token, 'your-secret-key', (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = { authenticateToken };
--- server.js
+++ server.js
@@ -1,24 +1,77 @@
-//Importing mongoDB :
-//Importing express :
+// //Importing mongoDB :
+// //Importing express :
 
+// const connectDB = require('./mongoDB/db');
+// const productRoutes = require('./routes/productRoutes');
+// const express = require('express');
+
+// const app = express();
+// app.use(express.json());
+
+// // Définir les routes et autres middleware :
+// connectDB().then(() => {
+//   app.listen(3000, () => {
+//     console.log('Node API is running on port 3000');
+//   });
+// });
+
+// app.use('/products', productRoutes);
+
+
+
+// Importing MongoDB and other required packages:
 const connectDB = require('./mongoDB/db');
 const productRoutes = require('./routes/productRoutes');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+const jwt = require('jsonwebtoken');
+const { applyMiddleware, createStore } = require('redux');
+const thunkMiddleware = require('redux-thunk').default;
+const { authenticateToken } = require('./middleware/auth');
 
 const app = express();
 app.use(express.json());
 
-// Définir les routes et autres middleware :
+// Create a server with http for socket.io:
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Define the routes and other middleware:
 connectDB().then(() => {
-  app.listen(3000, () => {
+  server.listen(3000, () => {
     console.log('Node API is running on port 3000');
   });
 });
 
-app.use('/products', productRoutes);
+// Apply Redux middleware:
+const store = createStore(() => {}, applyMiddleware(thunkMiddleware));
 
+// Socket.io configuration:
+io.use((socket, next) => {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    jwt.verify(socket.handshake.query.token, 'your-secret-key', (err, decoded) => {
+      if (err) return next(new Error('Authentication error'));
+      socket.decoded = decoded;
+      next();
+    });
+  } else {
+    next(new Error('Authentication error'));
+  }
+}).on('connection', (socket) => {
+  console.log('New socket connection:', socket.id);
+  // Handle socket events here
+});
 
+// Middleware to include authentication token in request headers
+app.use((req, res, next) => {
+  const token = req.header('Authorization');
+  if (token) {
+    req.headers.authorization = token;
+  }
+  next();
+});
 
+app.use('/products', authenticateToken, productRoutes);
 
-
-
+module.exports = app;
